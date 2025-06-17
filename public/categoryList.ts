7ff6e7cf27d0dd499ab18e1051061d327db91cd2
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-06-17 20:23:43 +0530

    category-couse-added

---
+++ public/categoryList.ts
@@ -0,0 +1,16 @@
+const categoryList = [
+  "Engineering & Technology",
+  "Medical & Health Sciences",
+  "Management & Business",
+  "Arts & Humanities",
+  "Commerce & Finance",
+  "Science & Applied Sciences",
+  "Education",
+  "Mass Communication & Media",
+  "Hospitality & Tourism",
+  "Design, Animation & Creative Arts",
+  "Computer Applications & IT",
+  "Law"
+];
+
+export default categoryList;
